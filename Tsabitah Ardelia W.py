46cf1288846d5fc5d3b8aeb58d7335e91cb99fc2
Tsabitah Ardelia Wahyudi <tsabitah365@gmail.com>
2025-11-05 11:52:26 +0700

    Create Tsabitah Ardelia W.py
    Membuat Dictionary

---
+++ Tsabitah Ardelia W.py
@@ -0,0 +1,12 @@
+# PENGENALAN DICTIONARY #
+
+Kucing = {
+    "Nama": "Mochii",
+    "Umur": 3,
+    "Ras": "Kampung",
+    "Betina": True,
+    "Hobi": ("Memanjat pohon", "Menggigit", "Makan es krim"),
+    "Status": "MeningsoyT-T"
+}
+
+print(Kucing)
